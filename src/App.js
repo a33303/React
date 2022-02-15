aa03dfe6325a609d5bd26ffe4ce618a0fe513dc6
a33303 <a33303@mail.ru>
2022-02-16 00:59:08 +0300

    HW1 1. Настройка Babel & Webpack для React-проекта 2. Компонент создан отдельно, но при импорте ошибка, решаю проблему с наставником (включил в Арр) 3. Сделано 4. Сделано (ошибка при импорте) 5. Сделано

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import Message from "./components/Message" // Ошибка при импорте закомитил - пока в поиске решения
-
-function Message(props) {
-    return (
-        <div>
-            <h1>Hello, {props.text}!</h1>
-            <h2>Lesson 1, React JS -GB </h2>
-        </div>
-    )
-}
+import React from "react"
+import Messages from "./components/Messages"
 
 function App() {
     return (
         <div>
-            <Message text="Aleksandr" />
-            <Message text="Ivan" />
-            <Message text="Nadya" />
+            <Messages text="Aleksandr" />
+            <Messages text="Ivan" />
+            <Messages text="Nadya" />
         </div>
     )
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+export default App
